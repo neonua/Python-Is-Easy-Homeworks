f42a032c151521ce1e872c2f1074f5d0eaf4366d
oleh <lovemybeard@gmail.com>
2019-03-02 20:05:05 +0200

    variables homework

---
+++ homework_1_variables.py
@@ -0,0 +1,26 @@
+"""
+This file describes possible attributes of a specific song, author's vision
+"""
+
+# List of all attributes
+name = 'Beautiful Lie'  # name of the song
+author = 'YOAV'  # author of the song
+album = 'Charmed and Strange'  # album of the song
+genre = 'Alternative'  # genre of the song
+release_year = 2008  # release year of the song
+duration = 5.09  # duration of the song as minutes.seconds
+duration_in_seconds = 309  # duration of the song in seconds
+link_to_albums_cover = '<link>'  # link to the cover of the album
+link_to_songs_cover = '<link>'  # link to the cover of the song
+
+# Printing part
+print('Following are attributes of a song:')
+print(f'Name - {name}')
+print(f'Author - {author}')
+print(f'Album - {album}')
+print(f'Genre - {genre}')
+print(f'Release year - {release_year}')
+print(f'Duration - {duration}')
+print(f'Duration in seconds - {duration_in_seconds}')
+print(f'Link to cover of an album - {link_to_albums_cover}')
+print(f'Link to cover of a song - {link_to_songs_cover}')
